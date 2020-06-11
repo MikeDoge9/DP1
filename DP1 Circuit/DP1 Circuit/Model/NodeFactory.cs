79aa6248a3fdcc079d12ba6f08abc1e5cde8ef03
MikeDoge9 <mike_damen@home.nl>
2020-06-11 14:04:01 +0200

    Classes

---
+++ DP1 Circuit/DP1 Circuit/Model/NodeFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP1_Circuit.Model
+{
+    public class NodeFactory
+    {
+        private static Dictionary<string, Type> nodes;
+
+
+        public NodeFactory() => nodes = new Dictionary<string, Type>();
+
+        public static void Register(string key, Type c)
+        {
+            nodes.Add(key, c);
+        }
+    }
+}
---
+++ DP1 Circuit/DP1 Circuit/Model/Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP1_Circuit.Model
+{
+    public class Output
+    {
+        public void Register() => NodeFactory.Register("OUTPUT", typeof(Output));
+    }
+}
